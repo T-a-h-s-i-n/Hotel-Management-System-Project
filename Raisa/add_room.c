4aa30a2b703c14a47b9ecd94191cf2ea7e461bbb
T-a-h-s-i-n <srtahsin32@gmail.com>
2025-01-04 23:48:54 +0600

    add_room.c

--- Raisa/add_room.c
+++ Raisa/add_room.c
@@ -2,7 +2,7 @@
 void AddRoom()
 {
     int roomNumber, floor, basePrice, isOccupied;
-    char type[20], view[20];
+    char type[20], view[20], ac_type[20];
 
     if (roomCount < MAX_ROOMS) // Ensure roomCount is less than MAX_ROOMS
     {
@@ -15,6 +15,8 @@ void AddRoom()
         scanf("%s", type);
         printf("Enter View (e.g., Sea, City): ");
         scanf("%s", view);
+        printf("Enter AC type (e.g., AC, Non AC): ");
+        scanf("%s", ac_type);
         printf("Enter Base Price: ");
         scanf("%d", &basePrice);
         printf("Is the room occupied? (1 for Yes, 0 for No): ");
@@ -27,14 +29,15 @@ void AddRoom()
         hotelRooms[roomCount].isOccupied = isOccupied;
         strcpy(hotelRooms[roomCount].type, type);
         strcpy(hotelRooms[roomCount].view, view);
+        strcpy(hotelRooms[roomCount].ac_type, ac_type);
 
         roomCount++;
         printf("\n\n  ---- Room added successfully.------\n");
-        printf("Room Number: %d\nFloor: %d\nRoom Type: %s\nView: %s\nBase Price: %d\nOccupied: %d\n",
-               roomNumber, floor, type, view, basePrice, isOccupied);
+        printf("Room Number: %d\nFloor: %d\nRoom Type: %s\nView: %s\nAC Type: %s\nBase Price: %d\nOccupied: %d\n",
+               roomNumber, floor, type, view, ac_type, basePrice, isOccupied);
     }
     else
     {
-        printf("Cannot add more rooms. Maximum capacity reached.\n");
+        printf("Sorry, no more room available!We are Fully booked! Please check back later.\n");
     }
 }
